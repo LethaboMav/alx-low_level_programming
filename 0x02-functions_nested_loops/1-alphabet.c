d80f981581732d55718d40087bc78ce72d11cdf3
LethaboMav <Lethabomavhungu1@gmail.com>
2023-02-22 07:56:17 +0000

    Printing alphabets in lowercases

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - print all alphabet in lowecase
+ */
+void print_alphabet(void)
+{
+	char awhile;
+
+	awhile = 'a';
+	while (a < 'z'
+	
+	
+		{
+		_putchar(awhile);
+		awhile++;
+	
+		}
+	_putchar('\n');
+	return;
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print the alphabet x10, in lowercase,
+ * followedba a new line
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i;
+
+	i =0;
+
+	while(i < 10)
+	{
+		ch = 'a'
+		while(ch <= 'z')
+		{
+			putchar(ch)
+			ch++;
+		}
+		_putchar('\n')
+		i++;
+	}
+}

f73e9125b589855aae4c52ba5406c64d54fd8de2
LethaboMav <Lethabomavhungu1@gmail.com>
2023-03-21 11:06:18 +0000

    Add files

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rot13 - Encodes a string using rot13.
+ * @s: The string.
+ *
+ * Return: A pointer to the string.
+ */
+
+char *rot13(char *s)
+{
+  char *ret = s;
+  char hf[] = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+  char he[] = "NnOoPpQqRrSsTtUuVvWwXxYyZzAaBbCcDdEeFfGgHhIiJjKkLlMm";
+  char *ptr, *ptr1;
+
+  while (*s)
+    {
+      for (ptr = hf, ptr1 = he; *ptr; ptr++, ptr1++)
+	if (((*s > 64 && *s < 91) || (*s > 96 && *s < 123)) && *s == *ptr)
+	  {
+	    *s = *ptr1;
+	    break;
+	  }
+      s++;
+    }
+  return (ret);
+}

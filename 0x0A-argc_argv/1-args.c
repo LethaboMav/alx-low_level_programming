e2593355baeda13bbe9ca247dd21dd0033185042
LethaboMav <Lethabomavhungu1@gmail.com>
2023-03-22 10:02:52 +0000

    Add New files

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - program prints no of arguments passed to it.
+ * @argc: agrument count to main.
+ * @argv: arguments to main
+ *
+ * Return: 0 always.
+ */
+
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+  printf("%d\n", argc - 1);
+  return (0);
+}
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print each argument on a newline
+ * @argc: argument count to main.
+ * @argv: array of pointers to char(arguments).
+ *
+ * Return: 0 always.
+ */
+
+int main(int argc, char *argv[])
+{
+  int i;
+  char *endptr;
+  int sum = 0;
+
+  if (argc == 1)
+    printf("0\n");
+  for (i = 1; argv[i] != NULL; i++)
+    {
+      sum += strtol(argv[i], &endptr, 10);
+      if (*endptr)
+	{
+	  printf("Error\n");
+	  return (1);
+	}
+    }
+  if (argc > 1)
+    printf("%d\n", sum);
+  return (0);
+}
